3c403b1a58b00796c8bcf782f57184dd8c1d820e
minhKhoa <trantuanminhkhoa2004@gmail.com>
2024-03-11 21:51:28 +0700

    nhanh a - change need to help

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ export default function Header(){
         <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          {description} React concepts you will need for almost any app you are
+          {description} React concepts you will help for almost any app you are
           going to build now!
         </p>
       </header>
