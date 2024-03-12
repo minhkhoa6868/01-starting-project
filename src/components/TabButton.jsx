477b7bddafd9e4f70e4a4923417063210db86935
minhKhoa <trantuanminhkhoa2004@gmail.com>
2024-03-12 22:24:35 +0700

    tab button - delete id "btn"

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
 export default function TabButton({children, onSelect}) {
     return (
         <li>
-            <button onClick={onSelect} id="btn">{children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     );
 }
